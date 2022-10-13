e00804ed79b183e14374b0394b87288e3b108ef9
mikkelsonmatwit <mikkelsonm@wit.edu>
2022-10-12 21:14:05 -0400

    solution to part a (first try)

--- src/parta.c
+++ src/parta.c
@@ -11,5 +11,25 @@
  */
 int main(int argc, char* argv[]) {
     // TODO: Complete this code and document
+    //check if there is 1 argument
+    if(argc != 2) {
+        printf("USAGE:/n/tparta WORD");
+        exit(1);
+    } else {
+        int fork_ret = fork();
+        if(fork_ret == 0) {
+            int status = execv("/usr/bin/grep", argv);
+            exit(status);
+        } else if(fork_ret > 0) {
+            int status;
+            wait(&status);
+            if(status == 0) {
+                printf("%s found", argv[1]);
+            } else {
+                printf("%s not found", argv[1]);
+            }
+        }
+
+    }
     return 0;
 }
